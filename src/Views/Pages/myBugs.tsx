53f743e639a0603799afa97965db039a94cf2bd7
vorsakha <andreitf.dev@gmail.com>
2021-08-10 10:11:28 -0300

    myBugs.tsx - add filter component

--- src/Views/Pages/myBugs.tsx
+++ src/Views/Pages/myBugs.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { BsToggleOff } from "@react-icons/all-files/bs/BsToggleOff";
-import { BsToggleOn } from "@react-icons/all-files/bs/BsToggleOn";
-import { filter } from "../../Controllers/Redux/bugSlice";
 import BugView from "../Components/Bug view/bugView";
 import LoadingSpinner from "../Components/loading";
 import BugCard from "../Components/bugCard";
-import { useAppDispatch, useAppSelector } from "../../Controllers/utils/hooks";
+import { useAppSelector } from "../../Controllers/utils/hooks";
+import Filter from "../Components/common/filter";
 
 // Types
 type MyBugsFormTypes = {
@@ -18,13 +16,11 @@ const MyBugs: React.FC = () => {
     name: "",
     isDisplayed: false,
   });
-  const [filteredArray, setFilteredArray] = useState<any[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
-  const [allBugsCount, setCount] = useState<number>(0);
 
-  const dispatch = useAppDispatch();
-  const { bugs, loading, isFiltered } = useAppSelector((state) => state.bugs);
-  const { auth } = useAppSelector((state) => state);
+  const { bugs, loading, isFiltered, myBugs, filteredArray } = useAppSelector(
+    (state) => state.bugs
+  );
 
   const handleName = (
     name: string,
@@ -36,28 +32,6 @@ const MyBugs: React.FC = () => {
     });
   };
 
-  const myBugs = bugs.filter(
-    (b: { assigned: string }) => b.assigned === auth.user
-  );
-
-  const handleFilterBugs = () => {
-    const pendingBugs = myBugs.filter(
-      (b: { status: string }) => b.status === "pending"
-    );
-
-    setFilteredArray(pendingBugs);
-    dispatch(filter());
-  };
-
-  useEffect(() => {
-    const pendingBugs = myBugs.filter(
-      (b: { status: string }) => b.status === "pending"
-    );
-
-    setFilteredArray(pendingBugs);
-    // eslint-disable-next-line
-  }, [bugs]);
-
   useEffect(() => {
     if (loading) {
       setLoading(true);
@@ -66,35 +40,11 @@ const MyBugs: React.FC = () => {
     }
   }, [loading]);
 
-  useEffect(() => {
-    setCount(filteredArray.length);
-  }, [filteredArray]);
-
   return (
     <div className="p-6 pt-10 max-w-screen-xl m-auto">
       {isLoading && <LoadingSpinner />}
       <h1 className="text-center text-3xl font-medium mb-4">All My Bugs</h1>
-      <div className="flex flex-row items-center mb-4">
-        Show Unresolved bugs
-        <span
-          className="font-bold text-white text-xs rounded-full bg-gray-700 flex items-center justify-center font-mono mr-1 ml-1"
-          style={{ height: "20px", width: "20px" }}
-        >
-          {allBugsCount}
-        </span>
-        :{" "}
-        <button
-          className="flex flex-row px-2 py-1 rounded ring-black ring-opacity-5 transition ease-in-out disabled:opacity-20"
-          onClick={handleFilterBugs}
-          disabled={myBugs.length === 0}
-        >
-          {!isFiltered ? (
-            <BsToggleOff className="text-gray-700 text-2xl" />
-          ) : (
-            <BsToggleOn className="text-gray-700 text-2xl" />
-          )}
-        </button>
-      </div>
+      <Filter />
       <div className="flex flex-wrap gap-4 justify-center col-span-2">
         {!isFiltered &&
           bugs !== null &&
